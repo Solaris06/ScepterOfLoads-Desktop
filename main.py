20bfb2b6ce4b65c49b25e03bbbd82ba107bd7e2d
Ethan Robards <yourtooslow@gmail.com>
2020-11-05 11:19:53 -0600

    first release?

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import subprocess, json, datetime, sys, csv, time, argparse
+
+import splits as splits
 import srcomapi.datatypes as dtypes
 import srcomapi
 import ffmpeg
@@ -27,6 +29,8 @@ def matchfilter(idict):
     fmt = list(filter(lambda f: f.get('width') == 1280 and f.get('ext') == 'mp4', formats))
     if httplink == "":
         httplink = fmt[0]['url']
+        j = open('infojson.json','w')
+        json.dump(idict, j, indent=2)
         return "Ye"
     return "Nah"
 yt_opts = {"skip_download": True, "match_filter": matchfilter}
@@ -39,43 +43,56 @@ parser.add_argument("gamelocation", type=str, help="The position of your game fo
 parser.add_argument("--splitsio", type=str, nargs='?', help="The 4-character id of the splits.io associated with this run.  Optional, but recommended.")
 parser.add_argument("--splitstext", type=str, nargs='?', help="The path to a text file with each split on a new line, formatted as hh:mm:ss.xxx.\nAll values must have trailing zeroes (05.089).")
 parser.add_argument("--output", type=str, nargs="?", default="output.csv", help="The results filename. Will be output in .csv form.")
+parser.add_argument("--manual", type=str, nargs="?", help="""The path to the csv file containing results. provided no splits.io id is specified.  Format your .csv like this:
+First line: name,time,medals
+Other lines below:Mission 1,01:50.200,0
+All missions have 0 medals.  2 if you S-rank, 1 if you A-D rank, 0 if you gold medal skip.""")
 parser.add_argument("--sonly", type=bool, default="false")
 args = parser.parse_args(sys.argv[1:])
 runresp = None
 category = ""
 rankadjust = []
-cat_ranks = {"Silver": [2, 2, 1, 2, 2, 1, 1, 2, 0, 1, 2, 2, 0, 0, 1, 2, 2, 0]}
-if args.splitsio:
-    sio_id = args.splitsio
-    runresp = requests.get("https://splits.io/api/v4/runs/{}".format(sio_id))
-    print("splits.io status code: {}".format(runresp.status_code))
-    runjson = runresp.json()
-    category = runjson['run']['category']['name']
-    for n, radj in cat_ranks.items():
-        if n in category:
-            rankadjust = radj
-            break
+cat_ranks = {"Silver": [2, 2, 1, 2, 2, 1, 1, 2, 0, 1, 2, 2, 0, 0, 1, 2, 2, 0],
+             "Sonic": [0, 1, 2, 1, 2, 0, 0, 0, 1, 1, 1, 2, 1, 2, 1, 1, 0, 0, 0, 1, 1, 0]}
 
+splitnames = []
+splits = []
 if not DEBUG:
+    runduration = -1
     if args.splitsio:
         sio_id = args.splitsio
         runresp = requests.get("https://splits.io/api/v4/runs/{}".format(sio_id))
         print("splits.io status code: {}".format(runresp.status_code))
         runjson = runresp.json()
-        duration_ms = runjson['run']['segments'][-1]['realtime_end_ms']
-        duration_s = str(duration_ms/1000)
-    else:
-        duration_s = str(60*60)
+        category = runjson['run']['category']['name']
+        splitnames = [runjson['run']['segments'][i]['display_name'] for i in range(len(runjson['run']['segments']))]
+        splits = [s['realtime_end_ms'] for s in runjson['run']['segments']]
+        runduration = splits[-1]/1000
+        for n, radj in cat_ranks.items():
+            if n in category:
+                rankadjust = radj
+                break
+    elif args.manual:
+        if not osp.exists(args.manual):
+            print("File {} not found, check pathname".format(args.manual))
+            raise FileNotFoundError
+        with open(args.manual) as csvf:
+            splits = []
+            cread = csv.DictReader(csvf)
+            for row in cread:
+                splitnames.append(row['name'])
+                microsecond_row = row['time'] + "000"
+                dtver = datetime.datetime.strptime(row['time'], "%M:%S.%f")
+                rankadjust.append(int(row['medals']))
+                splits.append(datetime.timedelta(minutes=dtver.minute, seconds=dtver.second, microseconds=dtver.microsecond))
+            runduration = splits[-1].total_seconds()
+
     load_intervals = []
-    splits = []
-    MANUAL = False
+
     if VIDEO:
         runvid_link = args.link
-
         vidw, vidh = map(int, args.resolution.split("x"))
         w,h,x,y = map(int, args.gamelocation.split(":"))
-        #ytdl_path = osp.join(osp.dirname(__file__), "youtube-dl")
-        #ytdl_path = "youtube-dl"
         starttime = time.time()
 
         if "http" in runvid_link:
@@ -91,8 +108,9 @@ if not DEBUG:
             xratio = str(x/vidw).format(floatfmt) + "*iw"
             yratio = str(y/vidh).format(floatfmt) + "*ih"
             runstream = ffmpeg.filter(runstream, "crop", **{"w": widthratio, "h": heightratio, "x": xratio, "y": yratio})
+        runstream = runstream.trim(start=args.start,end=args.start+runduration+3.5).filter("freezedetect", d=0.2, n="-53dB").output("-", format="null")
 
-            cstart = 0
+        cstart = 0
         cdur = 0
         loads = 0
         splitidx = 0
@@ -100,12 +118,10 @@ if not DEBUG:
         last_end = -1
         minute = 1
         loadinterval = []
-
-        runstream =  runstream.filter("freezedetect", d='0.2', n="-53dB").trim(start=5,end=15).output("outtest.mp4")
-        out, err = ffmpeg.run(runstream, capture_stderr=True)
-        for l in err.split("\n"):
-
-            if l.startswith("[freezedetect"):
+        ffresult = ffmpeg.run_async(runstream, pipe_stderr=True)
+        for l in ffresult.stderr:
+            l = l.decode("utf-8")
+            if "[freezedetect" in l:
                 if "start" in l and clean_freezeline(l) > (datetime.timedelta(minutes=49)).total_seconds():
                     break
                 elif "start" in l and len(loadinterval) == 0:
@@ -127,6 +143,8 @@ if not DEBUG:
                             print("Progress: {} minutes in".format(minute))
                             minute+= 1
                     loadinterval = []
+            else:
+                print(l)
         endtime = time.time()
         tdur = datetime.timedelta(seconds=(endtime-starttime))
         print("Total processing time: {}".format(str(tdur)))
@@ -175,16 +193,20 @@ if DEBUG:
 
 print("Done with footage, applying to splits...")
 if runresp is not None:
-    splitnames = [runjson['run']['segments'][i]['display_name'] for i in range(len(runjson['run']['segments']))]
     flamecoreidx = 10
-    splits = [s['realtime_end_ms'] for s in runjson['run']['segments']]
     splits = list(map(lambda s: datetime.timedelta(milliseconds=s), splits))
     formatted_splits = list(map(str, splits))
     seconds_splits = list(map(lambda s: s.total_seconds(), splits))
+elif args.manual:
+    for n in range(len(splitnames)):
+        if splitnames[n].lower() == "flame core":
+            flamecoreidx = n
+
     for i in range(len(splits)):
         if rankadjust[i] == 0:
             continue
         print("Split name: {}".format(splitnames[i]))
+        seconds_splits = list(map(lambda td: td.total_seconds(), splits))
         ctime = seconds_splits[i]
 
         preresults = ctime - 6 * rankadjust[i]
@@ -224,7 +246,7 @@ for i in range(len(seconds_splits)):
             break
     loadless_splits.append(seconds_splits[i] - loads)
 output_text = []
-outf = open('focus_4725.csv', 'w')
+outf = open('gordon_43_loadless.csv', 'w')
 outf.write("Split,RTA,Loadless,Loads\n")
 datlist = []
 for n in range(len(loadless_splits)):
@@ -232,8 +254,8 @@ for n in range(len(loadless_splits)):
         continue
 
     print("{}: {} / {}".format(splitnames[n], datetime.timedelta(seconds=seconds_splits[n]), datetime.timedelta(seconds=loadless_splits[n])))
-    datlist = [splitnames[n], datetime.timedelta(seconds=seconds_splits[n]), datetime.timedelta(seconds=loadless_splits[n])]
-    outf.write("{},{},{}\n".format(*datlist))
+    datlist = [splitnames[n], datetime.timedelta(seconds=seconds_splits[n]), datetime.timedelta(seconds=loadless_splits[n]), datetime.timedelta(seconds=seconds_splits[n]-loadless_splits[n])]
+    outf.write("{},{},{},{}\n".format(*datlist))
 outf.close()
 
 
