7de3b8add329ce1970f11c14f75831e589b7caea
Ethan Robards <yourtooslow@gmail.com>
2021-01-13 06:27:18 -0600

    bat typos

--- Scepter.bat
+++ Scepter.bat
@@ -7,7 +7,7 @@ IF "%%1"=="" (
 )
 set /p START="Enter the timestamp, in seconds, of when the run starts. Last frame before fadeout, no episode select arrows:"
 set /p RES="Enter the resolution of your OBS output as 1280x720, for example (or video, OBS is easier):"
-set /p DIMS="Enter the dimensions of your game footage within the video, as widthLheight:x:y, where x and y are where the top-left corner of the footage is:"
+set /p DIMS="Enter the dimensions of your game footage within the video, as width:height:x:y, where x and y are where the top-left corner of the footage is:"
 set /p MANUALCHOICE="D you have a splits.io ID of the run? [Y/N]:"
 if "%MANUALCHOICE%" == "Y" (
     set /p SIOID="Enter the 4-character splits.io id of the run."
